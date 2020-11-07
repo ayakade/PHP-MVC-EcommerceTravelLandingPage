d6c9b2483a7a7bf585043ec765605d0a38ea6dc0
ayakade <ayakade13@gmail.com>
2020-11-06 23:59:45 -0800

    Update layout.php

--- views/layout.php
+++ views/layout.php
@@ -1,6 +1,9 @@
-<?=$this->heroHTML?>
+<?=$this->heroTopHTML?>
 
-<div class="container col-12 col-lg-9">
+<div class="container col-12 col-lg-12">
     <!-- put contents here -->
     <?=$this->contentHTML?>
 </div><!-- .container / layout.php -->
+
+<?=$this->heroBottomHTML?>
+<?=$this->footerHTML?>
