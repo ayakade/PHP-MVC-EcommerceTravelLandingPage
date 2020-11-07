6648f445b82a6264d1510aba4c261afedb0463f8
ayakade <ayakade13@gmail.com>
2020-11-07 15:20:03 -0800

    Create success.php

---
+++ views/success.php
@@ -0,0 +1,11 @@
+<div class="success-container container">
+    <div class="success">
+        <img src="imgs/logo-blue.png" alt="AROUND THE WORLD">
+        <div class="title">
+            <h1>Thank you for sign up!</h1>
+            <p>You're on our list now!<br>Here's your 10% discount code for your first trip with us.<br><strong>TRAVEL10</strong><br><br>We will see you soon!</p>
+        </div><!-- .title -->
+
+        <a class="cta" href="index.php">Go back our website</a>
+    </div><!-- .success -->
+</div><!-- .success-container / success.php -->
