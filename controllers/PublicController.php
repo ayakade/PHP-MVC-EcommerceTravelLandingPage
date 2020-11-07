6a19a546c16ed105ec6d205e8641590e0644093a
ayakade <ayakade13@gmail.com>
2020-11-06 23:58:47 -0800

    updated function main()

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -6,13 +6,16 @@ Class PublicController extends Controller{
     // index page
     public function main()
     {
-        $this->loadView("views/heroTop.php", 1, "heroHTML"); 
+        $this->loadView("views/heroTop.php", 1, "heroTopHTML"); 
 
         $this->loadView("views/intro.php", 1, "contentHTML"); 
         $this->loadView("views/points.php", 1, "contentHTML"); 
         $this->loadView("views/form.php", 1, "contentHTML"); 
-        $this->loadView("views/heroBottom.php", 1, "contentHTML"); 
-        $this->loadView("views/footer.php", 1, "contentHTML"); 
+
+        $this->loadView("views/heroBottom.php", 1, "heroBottomHTML"); 
+
+        $this->loadView("views/footer.php", 1, "footerHTML"); 
+        
         $this->loadView("views/layout.php", 1, "content"); // save the results of this view, into $this->content
 
 		$this->loadLastView("views/main.php"); // final view
