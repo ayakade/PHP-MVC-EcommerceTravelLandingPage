4ac950655a7f7f9189585aa4ddd5b23d825070b0
ayakade <ayakade13@gmail.com>
2020-11-06 21:56:09 -0800

    Create admin-layout.php

---
+++ views/admin-layout.php
@@ -0,0 +1,9 @@
+<!-- nav -->
+<?=$this->navHTML?>
+
+<div class="container col-12 col-lg-9">
+    <!-- put contents here -->
+    <?=$this->contentHTML?>
+</div><!-- .container -->
+
+<!-- admin-layout.php -->	
