a05f59e3f918e0dbc8e78db72758ba15dee2bb4e
ayakade <ayakade13@gmail.com>
2020-11-06 23:59:52 -0800

    Update heroTop.php

--- views/heroTop.php
+++ views/heroTop.php
@@ -1,6 +1,9 @@
 <div class="heroTop">
     <div class="heroImage"></div>
     <div class="title">
-       
+       <img src="imgs/logo-blue.png" alt="AROUND THE WORLD">
+       <h1>Coming soon!</h1>
+       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula mauris sit amet nisl ultrices, vitae faucibus risus maximus.</p>
+       <a class="cta" href="#signupForm">Sign up</a>
     </div><!-- .title -->
 </div><!-- .heroTop / heroTop.php -->
