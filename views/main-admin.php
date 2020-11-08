7d22a6f6378503f893cb86edb431b532b4f738e5
ayakade <ayakade13@gmail.com>
2020-11-07 22:37:35 -0800

    Update main-admin.php

--- views/main-admin.php
+++ views/main-admin.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Admin Page</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/admin.css">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
