a8dc9b98c0ea4c985e735fee96e99d0b5867d435
ayakade <ayakade13@gmail.com>
2020-11-07 22:37:30 -0800

    added countdown clock

--- views/heroTop.php
+++ views/heroTop.php
@@ -5,6 +5,22 @@
     <div class="title">
        <img src="imgs/logo-blue.png" alt="AROUND THE WORLD">
        <h1>We are coming soon!</h1>
+
+       <div id="clock"> 
+            <div class="count"> 
+                <span class="days" id="day"></span> 
+                <div class="smalltext">Days</div> 
+            </div><!-- .count -->
+            <div class="count"> 
+                <span class="hours" id="hour"></span> 
+                <div class="smalltext">Hours</div> 
+            </div><!-- .count -->
+            <div class="count"> 
+                <span class="minutes" id="minute"></span> 
+                <div class="smalltext">Minutes</div> 
+            </div><!-- .count -->
+        </div><!-- .clock -->
+
        <p>Our website is lanching on Dec 13th.<br>Sign up for our VIP list now, get limited dicount code and save money for your next trip!</p>
        <a class="cta" href="#signup">Come Travel with Us!</a>
     </div><!-- .title -->
