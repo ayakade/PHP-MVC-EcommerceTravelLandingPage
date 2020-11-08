729acea166f9c8637b01d5707106feae01542a4f
ayakade <ayakade13@gmail.com>
2020-11-07 19:56:59 -0800

    Create accommodations.php

---
+++ views/accommodations.php
@@ -0,0 +1,30 @@
+<div class="accommodations col-12 col-lg-12">
+    <h2>Take a sneak peek of our unique accommodations</h2>
+
+    <div class="row">
+        <div class="accommodation col-12 col-lg-4">
+            <img src="imgs/accommodation-1.png" alt="Kep Mountain Lodge">
+            <div class="title">
+                <h3>Kep Mountain Lodge</h3>
+                <p><strong>City: Vancouver, Canada</strong><br><br>Kep Mountain Lodge is located on the edge of Kep National Park and a very short walk to a butterfly sanctuary. It’s the perfect gateway into the national park yet just a ten-minute drive from Kep Beach.<br><br>Kep Mountain Lodge is located on the edge of Kep National Park and a very short walk to a butterfly sanctuary. It’s the perfect gateway into the national park yet just a ten-minute drive from Kep Beach.</p>
+            </div><!-- .title -->
+        </div><!-- .accommodation -->
+
+        <div class="accommodation col-12 col-lg-4">
+            <img src="imgs/accommodation-2.png" alt="Tubohotel">
+            <div class="title">
+                <h3>Tubohotel</h3>
+                <p><strong>City: Mexico city, Mexico</strong><br><br>Each tube room is made from a nine-foot long industrial cement tube, previously used as a rainwater drain pipe. The tubes have since been refurbished, and their interiors have been painted with bright, warm, homey colors.<br><br>There’s plenty of storage space on the floor under the queen-sized beds (behind the simple yet effective welcome rug) and every tube room comes equipped with a lamp, a shelf behind the bed, a fan, two outlets, two windows (with screens), and curtains for privacy.</p>
+            </div><!-- .title -->
+        </div><!-- .accommodation -->
+
+        <div class="accommodation col-12 col-lg-4">
+            <img src="imgs/accommodation-3.png" alt="he AirShip">
+            <div class="title">
+                <h3>The AirShip</h3>
+                <p><strong>City: Wellington, New Zealand</strong><br><br>Designed by Roderick James Architects the interesting and innovative design creates a lovely internal space. Curved glass windows to the east and west frame views of the Isle of Mull, dramatic mountains and the sea.<br><br>Portholes provide exciting glimpses of the landscape beyond and the little balcony to the side is an idyllic place to eat alfresco when weather permits. Deer, sheep foxes and eagles graze and soar in the fields and woods around the AirShip.</p>
+            </div><!-- .title -->
+        </div><!-- .accommodation -->
+    </div><!-- .row -->
+
+</div><!-- .accommodations / accommodations.php -->
