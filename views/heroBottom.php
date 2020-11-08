b716e5345113e9f84e9f43d0f8ea8a282d07f5cd
ayakade <ayakade13@gmail.com>
2020-11-07 19:57:13 -0800

    Update heroBottom.php

--- views/heroBottom.php
+++ views/heroBottom.php
@@ -1,6 +1,7 @@
 <div class="heroBottom">
     <div class="heroImage"></div>
     <div class="title">
-       
+       <h2>We will see you on Dec 13th!</h2>
+       <p>Looking forward to traveling with you</p>
     </div><!-- .title -->
 </div><!-- .heroBottom / heroBottom.php -->
