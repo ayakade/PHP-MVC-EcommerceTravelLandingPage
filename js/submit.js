f29758c969169f882e931580bc55e649765819ef
ayakade <ayakade13@gmail.com>
2020-11-07 21:52:05 -0800

    Update submit.js

---
+++ js/submit.js
@@ -0,0 +1,35 @@
+// validation for submit form
+var Validate = function() {
+	var validate = this;
+	validate.element = document.getElementById("signUpForm");
+
+	validate.element.addEventListener("submit", function(event) {
+		
+		var error = false;
+		var fieldGroups = document.getElementsByClassName("required"); 
+		
+		for(var i=0; i<fieldGroups.length; i++)
+		{	
+			if(error)
+			{
+				event.preventDefault();
+			} 
+			
+			var field = fieldGroups[i].querySelector("input");
+			console.log(field);
+			var fieldValue = field.value;
+
+			if (fieldValue == "")
+			{
+				fieldGroups[i].classList.add("error");
+				error = true;
+			} else {
+				fieldGroups[i].classList.remove("error");
+			}
+		} // end of for(var i=0; i<fieldGroups.length; i++)
+		
+	}) // end of validate.element.addEventListener("submit", function(event)
+
+} // end of var Validate
+
+new Validate();
