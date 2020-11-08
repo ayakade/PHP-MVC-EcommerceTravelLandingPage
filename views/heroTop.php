039aba31381054565dda5d04e652944601b9acf1
ayakade <ayakade13@gmail.com>
2020-11-07 19:57:16 -0800

    Update heroTop.php

--- views/heroTop.php
+++ views/heroTop.php
@@ -1,9 +1,11 @@
 <div class="heroTop">
-    <div class="heroImage"></div>
+    <div class="filter">
+        <div class="heroImage"></div>
+    </div><!-- .filter -->
     <div class="title">
        <img src="imgs/logo-blue.png" alt="AROUND THE WORLD">
-       <h1>Coming soon!</h1>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vehicula mauris sit amet nisl ultrices, vitae faucibus risus maximus.</p>
-       <a class="cta" href="#signupForm">Sign up</a>
+       <h1>We are coming soon!</h1>
+       <p>Our website is lanching on Dec 13th.<br>Sign up for our VIP list now, get limited dicount code and save money for your next trip!</p>
+       <a class="cta" href="#signup">Come Travel with Us!</a>
     </div><!-- .title -->
 </div><!-- .heroTop / heroTop.php -->
