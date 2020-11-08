e95bf4462b075e3f420823bd311b13a9c419b285
ayakade <ayakade13@gmail.com>
2020-11-07 21:52:35 -0800

    Update success.php

--- views/success.php
+++ views/success.php
@@ -3,7 +3,7 @@
         <img src="imgs/logo-blue.png" alt="AROUND THE WORLD">
         <div class="title">
             <h1>Thank you for sign up!</h1>
-            <p>You're on our list now!<br>Here's your 10% discount code for your first trip with us.<br><strong>TRAVEL10</strong><br><br>We will see you soon!</p>
+            <p>You're on our VIP list now!<br>Here's your 10% discount code for your first trip with us.<br><strong>TRAVEL10</strong><br><br>We will see you soon!</p>
         </div><!-- .title -->
 
         <a class="cta" href="index.php">Go back our website</a>
