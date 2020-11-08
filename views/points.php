d05261d9d96a6d072567454382c5c3424a1e16a9
ayakade <ayakade13@gmail.com>
2020-11-07 19:57:22 -0800

    Update points.php

--- views/points.php
+++ views/points.php
@@ -1,3 +1,29 @@
 <div class="points">
-    <h2>this is point section</h2>
+    <h2>Reasons why you would love to travel with us</h2>
+    <div class="row">
+        <div class="point col-12 col-lg-4">
+            <i class="fas fa-globe-americas col-2"></i>
+            <div class="title col-9">
+                <h3>A global travel community</h3>
+                <p>We are available in 191+ countries, and our Community Standards help promote safety and belonging for everyone.</p>
+            </div><!-- .title -->
+        </div><!-- .point -->
+
+        <div class="point col-12 col-lg-4">
+            <i class="fas fa-hand-holding-heart col-2"></i>
+            <div class="title col-9">
+                <h3>Staffs who truly care</h3>
+                <p>We are all about the things that make you feel welcome wherever you go.</p>
+            </div><!-- .title -->
+        </div><!-- .point -->
+
+        <div class="point col-12 col-lg-4">
+            <i class="fas fa-user-clock col-2"></i>
+            <div class="title col-9">
+                <h3>We’re here for you—day or night</h3>
+                <p>Our 24/7 global support is available in 11 different languages and ready to help wherever you are.</p>
+            </div><!-- .title -->
+        </div><!-- .point -->
+
+    </div><!-- .row -->
 </div><!-- .points / points.php -->
