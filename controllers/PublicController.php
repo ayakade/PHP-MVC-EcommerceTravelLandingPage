ba646ee79bd9d598e021aee1dfa3e138150c9b04
ayakade <ayakade13@gmail.com>
2020-11-07 19:56:29 -0800

    Update PublicController.php

--- controllers/PublicController.php
+++ controllers/PublicController.php
@@ -8,7 +8,9 @@ Class PublicController extends Controller{
     {
         $this->loadView("views/heroTop.php", 1, "heroTopHTML"); 
 
+        $this->loadData(Cities::getRandCities(), "oCities");
         $this->loadView("views/intro.php", 1, "contentHTML"); 
+        $this->loadView("views/accommodations.php", 1, "contentHTML"); 
         $this->loadView("views/points.php", 1, "contentHTML"); 
         $this->loadView("views/form.php", 1, "contentHTML"); 
 
@@ -20,11 +22,30 @@ Class PublicController extends Controller{
 
 		$this->loadLastView("views/main.php"); // final view
     }
+
+    // sign up process
+    public function signup()
+    {
+        if($_POST["strFirstname"] && $_POST["strLastname"] && $_POST["strEmail"] && $_POST["strPhoneNumber"] && $_POST["strCountry"] && $_POST["strAge"] && $_POST["strImage"])
+		{
+			$con = DB::connect();
+			$sql = "INSERT INTO week8Customers (strFirstname, strLastName, strEmail, strPhoneNumber, strCountry, strAge) values ('".$_POST["strFirstname"]."', '".$_POST["strLastName"]."','".$_POST["strEmail"]."','".$_POST["strPhoneNumber"]."','".$_POST["strCountry"]."','".$_POST["strAge"]."','".$_POST["strImage"]."')";
+		
+			mysqli_query($con, $sql);
+
+			// if successed go to success page
+			$this->go("public", "success"); 
+		} else {
+			// if unsucseful 
+			$this->go("public", "main"); 
+		}
+    }
     
      // success page after submiting form
      public function success()
      {
+        $this->loadView("views/success.php", 1, "content"); // save the results of this view, into $this->content
  
-         $this->loadLastView("views/main.php"); // final view
+        $this->loadLastView("views/main.php"); // final view
      }
 }
