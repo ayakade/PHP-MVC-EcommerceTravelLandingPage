5de884f9d4f6743032fef10b72171c52911c15fe
ayakade <ayakade13@gmail.com>
2020-11-08 18:21:43 -0800

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,3 +1,8 @@
 <div class="footer">
+    <div class="sns">
+        <a href="#"><i class="fab fa-facebook-square"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
+        <a href="#"><i class="fab fa-instagram"></i></a>
+    </div><!-- .sns -->
     <p>© 2020 AROUND THE WORLD All rights reserved</p>
-</div>
+</div><!-- .footer / footer.php -->
